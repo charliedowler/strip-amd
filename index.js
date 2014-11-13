97fb273c7e47271b6445b5ef215d639451cca366
charliedowler <charliedowler15@gmail.com>
2014-11-13 23:49:24 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+(function() {
+  var staRegex = /define\(.*?\{/;
+  var midRegex = /return.*[^return]*$/;
+  var endRegex = /(}\);|}\))$/;
+
+  var stripper = function(contents) {
+    if (staRegex).test(contents)) {
+			contents = contents.replace(staRegex, '');
+      if (midRegex.test(contents)) {
+        contents = contents.replace(midRegex, '');
+      }
+      else if (endRegex.test(contents)) {
+        contents = contents.replace(endRegex, '');
+      }
+    }
+    return contents;
+  };
+
+  if (typeof module !== 'undefined' && 'exports' in module) {
+    module.exports = stripper;
+  } else if (typeof define !== 'undefined') {
+    define(function(require, exports, module) {
+      module.exports = stripper;
+    }, []);
+  } else if (typeof window !== 'undefined') {
+    window.strip_amd = stripper;
+  }
+})();
