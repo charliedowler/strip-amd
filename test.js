a0952197f09c2adc312a8a0b84a84890afe7be4b
charliedowler <charliedowler15@gmail.com>
2014-11-14 23:32:12 +0000

    setup travis

--- test.js
+++ test.js
@@ -15,7 +15,7 @@ var str3 = ['function() {',
             'return true;',
            '}'].join('');
 
-test("make sure the thingie is a thing", function (t) {
+test("test stripAMD", function (t) {
   t.equal(stripAMD(str), 'var testing = true;', "strip amd wrapper and return statement");
   t.equal(stripAMD(str2), 'var testing = true;', "strip amd wrapper and module.exports");
   t.equal(stripAMD(str3), str3, "only strip if define");
